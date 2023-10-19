f127a17ca85fbd75dbe3097c047a6e167a436cc1
foxstudent106092 <nik.datascience@gmail.com>
2023-10-19 22:35:01 +0100

    fix: new db logic to insert/update/check items

--- db/db.go
+++ db/db.go
@@ -9,30 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	tele "gopkg.in/telebot.v3"
-	"time"
 )
-
-// SubscriptionData represents data about subscriptions such as
-// a time subscription request was sent, chat and message ids,
-// user id and status of subscription
-type SubscriptionData struct {
-	Timestamp int64 `bson:"timestamp"`
-	MessageID int   `bson:"message_id"`
-	ChatID    int64 `bson:"chat_id"`
-	SenderID  int64 `bson:"sender_id"`
-	Status    int8  `bson:"status"`
-}
-
-func NewSubscriptionData(c tele.Context, status int8) *SubscriptionData {
-	return &SubscriptionData{
-		Timestamp: time.Now().Unix(),
-		SenderID:  c.Sender().ID,
-		MessageID: c.Message().ID,
-		ChatID:    c.Chat().ID,
-		Status:    status,
-	}
-}
 
 // DatabaseClient represents a service that interacts with a bot db
 type DatabaseClient struct {
@@ -42,10 +19,86 @@ type DatabaseClient struct {
 
 // DatabaseAccessor represents the methods for interacting with a bot db
 type DatabaseAccessor interface {
-	InsertSubscriptionDataToDB(doc interface{}) error
-	FindSubscriptionDataInDB(senderID int64) (*SubscriptionData, error)
-	CleanSubscriptionDataFromDB() error
+	InsertItemToDB(doc interface{}) error
+	FindItemInDb(userID int64) error
+	UpdateItemInDb(userID int64, recurringTime string, processed bool) error
 	CloseConnectionToDB() error
+}
+
+func NewDatabaseClient(cfg *config.DbCfg) DatabaseAccessor {
+	client, err := connectToDB(cfg)
+
+	if err != nil {
+		log.Error().Err(err).Send()
+		return nil
+	}
+
+	var databaseClient DatabaseAccessor = &DatabaseClient{
+		cfg:    cfg,
+		client: client,
+	}
+
+	return databaseClient
+}
+
+// UpdateItemInDb TODO refactor passing update arguments
+func (dc *DatabaseClient) UpdateItemInDb(userID int64, recurringTime string, processed bool) error {
+	collection := dc.client.Database(dc.cfg.Name).Collection(dc.cfg.SubsCollectionName)
+	filter := bson.D{{"user_id", userID}}
+	update := bson.M{
+		"$set": bson.M{
+			"processed":  processed,
+			"event.time": recurringTime,
+		},
+	}
+
+	_, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dc *DatabaseClient) FindItemInDb(userID int64) error {
+	collection := dc.client.Database(dc.cfg.Name).Collection(dc.cfg.SubsCollectionName)
+	filter := bson.D{{"user_id", userID}}
+
+	var subscriptionData interface{}
+	if err := collection.FindOne(context.TODO(), filter).Decode(&subscriptionData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dc *DatabaseClient) InsertItemToDB(doc interface{}) error {
+	collection := dc.client.Database(dc.cfg.Name).Collection(dc.cfg.SubsCollectionName)
+
+	if collection == nil {
+		err := errors.New(
+			fmt.Sprintf("error: db client failed to find collection \"%s\"", dc.cfg.SubsCollectionName),
+		)
+		return err
+	}
+
+	_, err := collection.InsertOne(context.TODO(), doc)
+	if err != nil {
+		return err
+	}
+
+	log.Info().Msg("Subscription was successfully stored in database!")
+
+	return nil
+}
+
+func (dc *DatabaseClient) CloseConnectionToDB() error {
+	err := dc.client.Disconnect(context.TODO())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func connectToDB(cfg *config.DbCfg) (*mongo.Client, error) {
@@ -67,78 +120,4 @@ func connectToDB(cfg *config.DbCfg) (*mongo.Client, error) {
 	log.Info().Msg("MongoDB client was successfully connected to the server!")
 
 	return client, nil
-}
-
-// NewDBClient initializes new DatabaseClient instance
-func NewDBClient(cfg *config.DbCfg) (DatabaseAccessor, error) {
-	client, err := connectToDB(cfg)
-
-	if err != nil {
-		log.Error().Err(err).Send()
-		return nil, err
-	}
-
-	var dbAccessor DatabaseAccessor = &DatabaseClient{
-		cfg:    cfg,
-		client: client,
-	}
-
-	return dbAccessor, nil
-}
-
-func (wbd *DatabaseClient) FindSubscriptionDataInDB(senderID int64) (*SubscriptionData, error) {
-	collection := wbd.client.Database(wbd.cfg.Name).Collection(wbd.cfg.SubsCollectionName)
-	filter := bson.D{{"sender_id", senderID}}
-
-	var subscriptionData *SubscriptionData
-
-	if err := collection.FindOne(context.TODO(), filter).Decode(&subscriptionData); err != nil {
-		return nil, err
-	}
-
-	return subscriptionData, nil
-}
-
-func (wbd *DatabaseClient) CleanSubscriptionDataFromDB() error {
-	collection := wbd.client.Database(wbd.cfg.Name).Collection(wbd.cfg.SubsCollectionName)
-
-	_, err := collection.DeleteMany(context.TODO(), bson.D{})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// CloseConnectionToDB closes clients connection to the server
-func (wbd *DatabaseClient) CloseConnectionToDB() error {
-	err := wbd.client.Disconnect(context.TODO())
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// InsertSubscriptionDataToDB inserts subscription data to db
-// !!!!!!!ADD EXISTS METHOD TO INTERFACE AND CALL InsertSubscriptionDataToDB
-// ONLY IF EXISTS METHOD RETURN FALSE
-func (wbd *DatabaseClient) InsertSubscriptionDataToDB(doc interface{}) error {
-	collection := wbd.client.Database(wbd.cfg.Name).Collection(wbd.cfg.SubsCollectionName)
-
-	if collection == nil {
-		err := errors.New(
-			fmt.Sprintf("error: db client failed to find collection \"%s\"", wbd.cfg.SubsCollectionName),
-		)
-		return err
-	}
-
-	_, err := collection.InsertOne(context.TODO(), doc)
-	if err != nil {
-		return err
-	}
-
-	log.Info().Msg("Subscription was successfully stored in database!")
-
-	return nil
 }
