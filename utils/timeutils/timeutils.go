48c6d1a8e9321cf2bff11d132b05a0ca31605895
foxstudent106092 <nik.datascience@gmail.com>
2023-10-19 08:44:55 +0100

    feat: parse time of type string as 24H format

--- utils/timeutils/timeutils.go
+++ utils/timeutils/timeutils.go
@@ -2,7 +2,18 @@ package timeutils
 
 import "time"
 
+const Layout24H = "15:04"
+
 // ConvertUnixTimestampToDate converts Unix Timestamp to date format (layout)
 func ConvertUnixTimestampToDate(timestamp int64, layout string) string {
 	return time.Unix(timestamp, 0).Format(layout)
+}
+
+func ParseTimeFormat(ts string) (*time.Time, error) {
+	t, err := time.Parse(Layout24H, ts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &t, nil
 }
